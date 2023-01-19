c09c9d9c808d0c9d90d3902567c9d178cf76a75d
nyemphie26 <s.hidayatullah1726@gmail.com>
2023-01-19 20:25:11 +0700

    add Fonts Family

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -4,6 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ env('APP_NAME') }}</title>
+    {{-- Fonts Family --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Courgette&family=Open+Sans&display=swap" rel="stylesheet">
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> --}}
     <link rel="stylesheet" href="{{asset('assets/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{asset('assets/css/style.css') }}">
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -25,15 +25,15 @@
         </div>
         <div class="row justify-content-center">
             <div class="col-md-3 text-center text-white">
-                <h3 class="mb-4">Step 1</h3>
+                <h2 class="mb-4 text-primary">Step 1</h2>
                 <p>Lorem ipsum dolor sit amet.</p>
             </div>
             <div class="col-md-3 text-center text-white">
-                <h3 class="mb-4">Step 2</h3>
+                <h2 class="mb-4 text-primary">Step 2</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus iusto ipsum sit consequuntur ab porro!</p>
             </div>
             <div class="col-md-3 text-center text-white">
-                <h3 class="mb-4">Step 3</h3>
+                <h2 class="mb-4 text-primary">Step 3</h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, quis?</p>
             </div>
         </div>
@@ -45,71 +45,71 @@
         </div>
     </div>
 <div class="sectionBanner row justify-content-center align-items-center">
-    <div class="col-5 border text-end">sdfsd</div>
-    <div class="col-5 border">
+    <div class="col-5  text-end">sdfsd</div>
+    <div class="col-5 ">
         <h2 class="mb-4">Proudly Present To You</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ipsum, dolorem animi incidunt modi, illo excepturi doloremque, quis illum nulla aliquid molestiae. Natus praesentium, incidunt temporibus minus accusamus at corrupti illo voluptatum veniam quidem? Velit aut, laboriosam laudantium ducimus cumque doloribus excepturi! Accusamus, minima temporibus?</p>
         <button class="btn btn-outline-light btn-lg">Learn More how to order</button>
     </div>
 </div>
-    <div class="parallax d-grid align-items-center py-5 gap-1 border" style="background-image: url('{{ asset('assets/images/3.jpg') }}')">
-        <div class="row justify-content-center border">
-            <div class="col-md-10 text-center text-white border">
+    <div class="parallax d-grid align-items-center py-5 gap-1 " style="background-image: url('{{ asset('assets/images/3.jpg') }}')">
+        <div class="row justify-content-center ">
+            <div class="col-md-10 text-center text-white ">
                 <h2 class="mb-4">Wedding Gown, Cleaning and Finishing</h2>
             </div>
         </div>
-        <div class="row justify-content-center border">
-            <div class="col-md-5 text-center text-white border">
+        <div class="row justify-content-center ">
+            <div class="col-md-5 text-center text-white ">
                 <h3 class="mb-4">Pre-Wedding</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus iusto ipsum sit consequuntur ab porro!</p>
             </div>
-            <div class="col-md-5 text-center text-white border">
+            <div class="col-md-5 text-center text-white ">
                 <h3 class="mb-4">Post-Wedding</h3>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, quis?</p>
             </div>
         </div>
-        <div class="row justify-content-center center border">
-            <div class="col-md-10 text-center text-white border">
+        <div class="row justify-content-center center ">
+            <div class="col-md-10 text-center text-white ">
                 <h2 class="mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h2>
                 <button class="btn btn-secondary btn-lg">Register Now</button>
             </div>
         </div>
     </div>
 <div class="section d-grid align-items-center">
-    <div class="row justify-content-center border">
-        <div class="col-3 text-center border">
+    <div class="row justify-content-center ">
+        <div class="col-3 text-center ">
             <h1 class="text-success">Icon</h1>
             <h3 class="text-secondary">Service 1</h3>
             <p class="text-secondary">Lorem ipsum dolor sit amet.</p>
             <button type="button" class="btn btn-link text-secondary">Click here </button>
         </div>
-        <div class="col-3 text-center border">
+        <div class="col-3 text-center ">
             <h1 class="text-success">Icon</h1>
             <h3 class="text-secondary">Service 2</h3>
             <p class="text-secondary">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, nostrum.</p>
             <button type="button" class="btn btn-link text-secondary">Click here </button>
         </div>
-        <div class="col-3 text-center border">
+        <div class="col-3 text-center ">
             <h1 class="text-success">Icon</h1>
             <h3 class="text-secondary">Service 3</h3>
             <p class="text-secondary">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, nostrum.</p>
             <button type="button" class="btn btn-link text-secondary">Click here </button>
         </div>
     </div>
-    <div class="row justify-content-center border">
-        <div class="col-3 text-center border">
+    <div class="row justify-content-center ">
+        <div class="col-3 text-center ">
             <h1 class="text-success">Icon</h1>
             <h3 class="text-secondary">Service 4</h3>
             <p class="text-secondary">Lorem ipsum dolor sit amet.</p>
             <button type="button" class="btn btn-link text-secondary">Click here </button>
         </div>
-        <div class="col-3 text-center border">
+        <div class="col-3 text-center ">
             <h1 class="text-success">Icon</h1>
             <h3 class="text-secondary">Service 5</h3>
             <p class="text-secondary">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, nostrum.</p>
             <button type="button" class="btn btn-link text-secondary">Click here </button>
         </div>
-        <div class="col-3 text-center border">
+        <div class="col-3 text-center ">
             <h1 class="text-success">Icon</h1>
             <h3 class="text-secondary">Service 6</h3>
             <p class="text-secondary">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, nostrum.</p>
