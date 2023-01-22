681f90c5cd69c8144233c07968070cb1bb2e97be
nyemphie26 <s.hidayatullah1726@gmail.com>
2023-01-22 08:46:54 +0700

    Update loader style and timeload

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -16,39 +16,42 @@
   </head>
   <body>
     <div class="loader loader-active "></div>
-    @include('Partials.navbar')
-    @yield('content')
+    <div class="section" style="display: none">
+      @include('Partials.navbar')
+      @yield('content')
 
-    <div class="section footer d-grid bg-dark align-items-center" style="height: 20vh">
-      <div class="d-flex justify-content-center text-center column-gap-3">
-          <a href="#">
-              <div class="social-box bg-primary">
-                  <i class="fa-brands fa-facebook-f" style="color: white"></i>
-              </div>
-          </a>
-          <a href="#">
-              <div class="social-box bg-primary">
-                  <i class="fa-brands fa-twitter" style="color: white"></i>
-              </div>
-          </a>
-          <a href="#">
-              <div class="social-box bg-primary">
-                  <i class="fa-brands fa-instagram" style="color: white"></i>
-              </div>
-          </a>
+      <div class="section footer d-grid bg-dark align-items-center" style="height: 20vh">
+        <div class="d-flex justify-content-center text-center column-gap-3">
+            <a href="#">
+                <div class="social-box bg-primary">
+                    <i class="fa-brands fa-facebook-f" style="color: white"></i>
+                </div>
+            </a>
+            <a href="#">
+                <div class="social-box bg-primary">
+                    <i class="fa-brands fa-twitter" style="color: white"></i>
+                </div>
+            </a>
+            <a href="#">
+                <div class="social-box bg-primary">
+                    <i class="fa-brands fa-instagram" style="color: white"></i>
+                </div>
+            </a>
+        </div>
       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script>
-      // const myTimeout = setTimeout(myGreeting, 500);
       
         window.onscroll = function() {scrollFunction()};
         window.addEventListener('scroll', reveal);
         window.addEventListener('load', loading);
         
-        
         function loading() {
-          document.querySelector(".loader").classList.remove("loader-active");
+          setTimeout(() => {
+            document.querySelector(".loader").classList.remove("loader-active");
+            document.querySelector(".section").style.display = "block";
+          }, 1000);
         }
         
         
