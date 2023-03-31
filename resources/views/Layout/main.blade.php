0e735b3c6a457b3499e48d9fbc8db256f2933f54
nyemphie26 <s.hidayatullah1726@gmail.com>
2023-03-31 10:30:22 +0700

    update title v2

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{-- <title>{{ env('APP_NAME') }}</title> --}}
-    <title>update v1</title>
+    <title>update v2</title>
     {{-- Fonts Family --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
